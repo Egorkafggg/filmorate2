c3cf3787df3981485cecaa0b643c22c5c2f90782
Егорка <159724676+Egorkafggg@users.noreply.github.com>
2026-04-17 13:25:55 +0300

    Update FilmController.java

--- filmorate2/src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ filmorate2/src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -18,7 +18,7 @@ import java.util.Map;
 @Slf4j
 @RestController
 @RequestMapping("/film")
-public class FilmController {
+public class FilmController { 
 
     //private final static Logger log = LoggerFactory.getLogger(FilmController.class);
     private final Map<Long, Film> films = new HashMap<>();
@@ -27,27 +27,27 @@ public class FilmController {
 
 
     @PostMapping
-    public Film create(@Valid @RequestBody Film film){
+    public Film create(@Valid @RequestBody Film film){ 
         film.setId(++generateId);
         films.put(film.getId(), film);
         log.info("Добавлен фильм {}",film);
         return film;
-    }
+    } 
 
     @PutMapping
-    public Film upFilm(@Valid@RequestBody Film film){
+    public Film upFilm(@Valid@RequestBody Film film){ 
         if (film.getId() == 0 || films.containsKey(film.getId())){
             log.warn("Попытка обновления не существующего фильма",film.getId());
             throw new ValidateException("Фильм с Id " + film.getId() + " не найден");
-        }
+        } 
         films.put(film.getId(), film);
         log.info("Обновлен фильм {}",film.getId());
         return film;
-    }
+    } 
 
     @GetMapping
-    public Collection<Film> listFilms(){
+    public Collection<Film> listFilms(){ 
         log.info("Получен запрос на получение всех фильмов");
         return films.values();
-    }
-}
+    } 
+} 
